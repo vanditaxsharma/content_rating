c350fc0ba7d30c4a26ed6d942c848b3d2c23c0e0
vanditaxsharma <1912vanditaa@gmail.com>
2025-09-01 09:58:04 +0530

    Update ContentRating.jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -3,16 +3,40 @@ import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: 0,
+            dislikes: 0,
+            handleLike: () =>{
+this.setState((prevState)=>({
+    likes:prevState.likes +1
+}))
+            },
+            handleDislike: () => {
+this.setState((prevState)=>({
+    dislikes:prevState.dislikes +1
+}))
+            }
+        }
+    }
+    render() {
+        return (
+            <>
+                <div className='content-rating'>
+                    <p>Human psychology</p>
+                    <div className="rating-buttons">
+                        <button className="like-button" onClick={this.state.handleLike}>
+                            Like ({this.state.likes})
+                        </button>
+                        <button className="dislike-button" onClick={this.state.handleDislike}>
+                            disLike ({this.state.dislikes})
+                        </button>
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
 
 export default ContentRating;
